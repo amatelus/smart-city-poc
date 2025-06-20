5327bf2cb4f0a60be14bad82575b06f96e6fe2dd
solufa <solufa2020@gmail.com>
2025-06-20 22:35:20 +0900

    chore: remove inbucket

--- tests/setupMsw.ts
+++ tests/setupMsw.ts
@@ -6,7 +6,6 @@ import { TEST_BASE_URL, testEnvs } from './utils';
 export function setupMsw(): SetupServerApi {
   const server = setupServer(
     http.all(`${testEnvs().S3_ENDPOINT}/*`, passthrough),
-    http.all(`${testEnvs().INBUCKET_URL}/*`, passthrough),
     ...setupMswHandlers(),
     ...setupMswHandlers({ baseURL: TEST_BASE_URL }),
   );
